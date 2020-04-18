b1c8e5d5a7f10f35daedff9a62116cf48af84f41
Turner <amitur91@gmail.com>
2020-04-18 16:05:56 +0300

    Update ShowShare.js

--- front/decision_front/src/components/header/ShowShare.js
+++ front/decision_front/src/components/header/ShowShare.js
@@ -1,8 +1,7 @@
 
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { useTracked  } from '../../store'
-import Paper from '@material-ui/core/Paper'
+import { useTrackedState  } from '../../store'
 import {green} from '../GlobalVars'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { Button, Typography, IconButton } from '@material-ui/core'
@@ -33,18 +32,16 @@ const useStyles = makeStyles((theme) => ({
 export default function ShowShare({closeAction}) {
     const classes = useStyles();
 
-    const [ state, dispatch ] = useTracked()
+    const state = useTrackedState()
     const [ copied, setCopied ] = useState(false)
 
     let id = state.id
 
-    useEffect(() => {
-    
-        if (copied) {
-          setTimeout( () => setCopied(false), 3000 )
-        }
-            
-    }, [copied])
+    const handleCopied = () => {
+      setCopied(true)
+      setTimeout( () => setCopied(false), 3000 )
+    }
+
     
     return (
         <div style={{background:'rgba(0, 0, 0, 0.1)',padding:'15px',position:'relative'}}>
@@ -57,7 +54,7 @@ export default function ShowShare({closeAction}) {
                 <br/>
 
                 <CopyToClipboard text={id}
-                    onCopy={() => setCopied(true)}>
+                    onCopy={handleCopied}>
                     <Button className={classes.copyButton} endIcon={<Icon path={ICONS['Copy']} size={0.7} color="rgba(0, 0, 0, 0.3)"/>}>
                       <Typography style={{width:'100%'}}>
                       {copied? 'Copied!' : 'Copy to clipboard'}
