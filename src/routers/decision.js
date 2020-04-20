8a51358dac1ff2484b68cc68caf775e900c179b6
Raz <razik.gvili@gmail.com>
2020-04-20 18:46:49 +0300

    idd

--- src/routers/decision.js
+++ src/routers/decision.js
@@ -9,6 +9,14 @@ let _ = require('lodash')
 // middleware 
 const auth = require('../middleware/auth')
 
+const parseDate = dateString => {
+    const b = dateString.split(/\D+/);
+    const offsetMult = dateString.indexOf('+') !== -1 ? -1 : 1;
+    const hrOffset = offsetMult * (+b[7] || 0);
+    const minOffset = offsetMult * (+b[8] || 0);  
+    return new Date(Date.UTC(+b[0], +b[1] - 1, +b[2], +b[3] + hrOffset, +b[4] + minOffset, +b[5], +b[6] || 0));
+};
+
 
 // router.get('/decisions/:id', auth, (req, res) => {
 
@@ -175,11 +183,14 @@ router.patch('/decisions/:id', (req, res) => {
 
                 console.log("Deleting comment with id --> " + req.body.commentIdToDelete)
 
+                //let now = new Date()
                 decision.comments = decision.comments.filter((commentIter) => {
-                    console.log(commentIter._id)
-                    console.log(typeof commentIter._id)
-                    commentIter._id !== req.body.idToDelete}
-                )
+                    
+                    if (commentIter._id !== req.body.idToDelete) {
+                        return commentIter
+                    }
+
+                })
             } 
 
             if (update !== 'newComment' && update !== 'commentIdToDelete') {
