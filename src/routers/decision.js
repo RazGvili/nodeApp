c41ffa327fcfdac52525d9318550e9d3c33b0c15
Raz <razik.gvili@gmail.com>
2020-04-20 21:44:45 +0300

    logs

--- src/routers/decision.js
+++ src/routers/decision.js
@@ -5,44 +5,15 @@ const Decision = require('../models/decision')
 
 let _ = require('lodash')
 
-
 // middleware 
-const auth = require('../middleware/auth')
+//const auth = require('../middleware/auth')
 
 let log = require('../logger')
 
 
-// router.get('/decisions/:id', auth, (req, res) => {
-
-//     console.log("task ID from params--> " + req.params.id)
-//     console.log("user ID from auth--> " + req.user._id)
-
-//     const _id = req.params.id
-//     const user = req.user
-
-//     Decision.findOne({
-//         owner: user._id,
-//         _id
-//     }).then((decision) => {
-
-//         if (!decision) {
-//             return res.status(404).send({
-//                 "err": "decision not found"
-//             })
-//         }
-
-//         res.send(decision)
-
-//     }).catch((err) => {
-//         res.status(500).send(err)
-//     })
-
-// })
-
 router.get('/decisions/:id', (req, res) => {
 
     log.info("task ID from params--> %s",req.params.id)
-    //console.log("user ID from auth--> " + req.user._id)
 
     const _id = req.params.id
     //const user = req.user
@@ -67,64 +38,6 @@ router.get('/decisions/:id', (req, res) => {
     })
 
 })
-
-
-
-// router.get('/decisions', auth, (req, res) => {
-
-//     let match = {}
-//     const user = req.user
-    
-//     if (req.query.completed) {
-
-//         console.log("req.query--->")
-//         console.log(req.query)
-//         console.log("\n")
-        
-//         if (req.query.completed === 'true' || req.query.completed === 'false') {
-//             match.completed = req.query.completed === 'true'
-
-//         } else {
-//             console.log("invalid query param: " + req.query.completed)
-//         }
-    
-//     } else {
-//         console.log("req.query---> Non received ")
-//     }   
-
-//     console.log("user ID from auth--> " + user._id)
-
-//     user.populate({
-//         path: 'decisions',
-//         match,
-//         options: {
-//             sort: {
-//                 createdAt: -1
-//             }
-//         }
-
-//     }).execPopulate().then(() => {
-
-//         if (!user.decisions) {
-//             return res.status(404).send({
-//                 "err": "decisions not found"
-//             })
-//         }
-
-//         console.log("decisions --->")
-//         console.log(user.decisions)
-//         console.log("\n")
-
-//         res.send(user.decisions)
-
-//     }).catch((err) => {
-
-//         console.log(err)
-//         res.status(500).send({
-//             "err": err
-//         })
-//     })
-// })
 
 
 router.patch('/decisions/:id', (req, res) => {
@@ -198,48 +111,13 @@ router.patch('/decisions/:id', (req, res) => {
 
     }).catch((err) => {
 
-        console.log(err)
+        log.info({err: err})
         res.status(500).send({
             "err": "Update decision fail"
         })
     })
 
 })
-
-
-// router.post('/decisions', auth, (req, res) => {
-
-//     if (_.isEmpty(req.body)) {
-//         return res.status(400).send({
-//             "err": "body is empty"
-//         })
-//     }
-
-//     const decision = new Decision({
-//         ...req.body,
-//         owner: req.user._id
-//     })
-
-//     console.log("Decision object to save --->")
-//     console.log(decision)
-
-//     decision.save().then((decision) => {
-
-//         console.log("Decision object saved! \n")
-//         res.status(201).send(decision)
-
-//     }).catch((err) => {
-
-//         console.log("Decision object save failed! \n")
-//         console.log(err)
-//         res.status(400).send({
-//             err
-//         })
-
-//     })
-
-// })
-
 
 
 router.post('/decisions', (req, res) => {
@@ -278,6 +156,70 @@ router.post('/decisions', (req, res) => {
 
 
 
+// router.get('/decisions/:id', auth, (req, res) => {
+
+//     console.log("task ID from params--> " + req.params.id)
+//     console.log("user ID from auth--> " + req.user._id)
+
+//     const _id = req.params.id
+//     const user = req.user
+
+//     Decision.findOne({
+//         owner: user._id,
+//         _id
+//     }).then((decision) => {
+
+//         if (!decision) {
+//             return res.status(404).send({
+//                 "err": "decision not found"
+//             })
+//         }
+
+//         res.send(decision)
+
+//     }).catch((err) => {
+//         res.status(500).send(err)
+//     })
+
+// })
+
+
+
+// router.post('/decisions', auth, (req, res) => {
+
+//     if (_.isEmpty(req.body)) {
+//         return res.status(400).send({
+//             "err": "body is empty"
+//         })
+//     }
+
+//     const decision = new Decision({
+//         ...req.body,
+//         owner: req.user._id
+//     })
+
+//     console.log("Decision object to save --->")
+//     console.log(decision)
+
+//     decision.save().then((decision) => {
+
+//         console.log("Decision object saved! \n")
+//         res.status(201).send(decision)
+
+//     }).catch((err) => {
+
+//         console.log("Decision object save failed! \n")
+//         console.log(err)
+//         res.status(400).send({
+//             err
+//         })
+
+//     })
+
+// })
+
+
+
 // router.delete('/decisions/:id', auth, (req, res) => {
 
 //     const _id = req.params.id
@@ -305,6 +247,65 @@ router.post('/decisions', (req, res) => {
 //         console.log(err)
 //         res.status(500).send({
 //             "err": "Delete decision fail"
+//         })
+//     })
+// })
+
+
+
+
+// router.get('/decisions', auth, (req, res) => {
+
+//     let match = {}
+//     const user = req.user
+    
+//     if (req.query.completed) {
+
+//         console.log("req.query--->")
+//         console.log(req.query)
+//         console.log("\n")
+        
+//         if (req.query.completed === 'true' || req.query.completed === 'false') {
+//             match.completed = req.query.completed === 'true'
+
+//         } else {
+//             console.log("invalid query param: " + req.query.completed)
+//         }
+    
+//     } else {
+//         console.log("req.query---> Non received ")
+//     }   
+
+//     console.log("user ID from auth--> " + user._id)
+
+//     user.populate({
+//         path: 'decisions',
+//         match,
+//         options: {
+//             sort: {
+//                 createdAt: -1
+//             }
+//         }
+
+//     }).execPopulate().then(() => {
+
+//         if (!user.decisions) {
+//             return res.status(404).send({
+//                 "err": "decisions not found"
+//             })
+//         }
+
+//         console.log("decisions --->")
+//         console.log(user.decisions)
+//         console.log("\n")
+
+//         res.send(user.decisions)
+
+//     }).catch((err) => {
+
+//         console.log(err)
+//         res.status(500).send({
+//             "err": err
 //         })
 //     })
 // })
